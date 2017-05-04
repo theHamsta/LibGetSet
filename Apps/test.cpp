bca17b7e77336ca93b01d2a0f8252d6741e72a9f
Andre Aichert <aaichert@gmail.com>
2017-05-04 10:17:58 +0200

    major bugfix: m_path member of GetSetTabWidget was not set correctly (recently broken)

--- Apps/test.cpp
+++ Apps/test.cpp
@@ -11,67 +11,18 @@ GetSetGui::GetSetApplication g_app("Test");
 // Managing saving/loading parametzers and automatic GUI
 #include <GetSetGui/GetSetTabWidget.h>
 
-/// Pre-processing of X-ray projection images.
-struct PreProccess {
-	
-	/// Modifying intensities (first normalize, then apply bias and scale, then logarithm)
-	struct Intensity {
-		bool	normalize=false;
-		double	bias=0.0;
-		double	scale=1.0;
-		bool	apply_log=false;
-	} intensity;
-
-	/// Low-pass filter. Applied after intensity changes.
-	struct Lowpass {
-		double gaussian_sigma=1.84;
-		int    half_kernel_width=5;
-	} lowpass;	
-	
-	/// Modifying geometry
-	struct Geometry {
-		bool	flip_u=false;
-		bool	flip_v=false;
-	} geometry;
-
-	/// Declare default values.
-	void declareGUI(GetSetSection& section)
-	{
-		section.key<bool>  ("Intensity/Per Pixel/Normalize")=intensity.normalize;
-		section.key<double>("Intensity/Per Pixel/Bias")=intensity.bias;
-		section.key<double>("Intensity/Per Pixel/Scale")=intensity.scale;
-		section.key<bool>  ("Intensity/Per Pixel/Apply Minus Logarithm")=intensity.apply_log;
-		section.subsection("Intensity/Per Pixel").setGrouped();
-			
-		section.key<double>("Intensity/Lowpass Filter/Gaussian Sigma")=lowpass.gaussian_sigma;
-		section.key<int>   ("Intensity/Lowpass Filter/Half Kernel Width")=lowpass.half_kernel_width;
-		section.subsection ("Intensity/Lowpass Filter").setGrouped();
-
-		section.key<bool>  ("Geometry/Flip u-Axis")=geometry.flip_u;
-		section.key<bool>  ("Geometry/Flip v-Axis")=geometry.flip_v;
-		section.subsection("Geometry").setGrouped();
-
-	}
-	
-	// Retreive current values from GUI
-	void retreiveFromGUI(GetSetSection& section)
-	{
-		intensity.normalize=section.key<bool>("Intensity/Normalize");
-		intensity.bias=section.key<double>("Intensity/Bias");
-		intensity.scale=section.key<double>("Intensity/Scale");
-		intensity.apply_log=section.key<bool>("Intensity/Apply Minus Logarithm");
-		lowpass.gaussian_sigma=section.key<double>("Intensity/Lowpass Filter/Gaussian Sigma");
-		lowpass.half_kernel_width=section.key<int>("Intensity/Lowpass Filter/Half Kernel Width");
-		geometry.flip_u=section.key<bool>("Geometry/Flip u-Axis");
-		geometry.flip_v=section.key<bool>("Geometry/Flip v-Axis");
-	}
-};
-
-
 void gui(const std::string& section, const std::string& key)
 {
 	std::cout << key << std::endl;
 
+	if (section=="Test/Range")
+	{
+		std::cout << "Reset range.\n";
+		GetSetGui::RangedDouble("Test/Value")
+			.setMin(GetSet<int>("Test/Range/Min"))
+			.setMax(GetSet<int>("Test/Range/Max"))
+			;
+	}
 
 //	g_app.saveSettings();
 }
@@ -79,13 +30,14 @@ void gui(const std::string& section, const std::string& key)
 int main(int argc, char **argv)
 {
 
-	PreProccess().declareGUI(GetSetSection("Pre-Processing"));
+	GetSet<int>("Test/Range/Min")=5;
+	GetSet<int>("Test/Range/Max")=9;
 
+	GetSetGui::RangedDouble("Test/Value")
+		.setMin(GetSet<int>("Test/Range/Min"))
+		.setMax(GetSet<int>("Test/Range/Max"))
+		;
 	g_app.init(argc,argv,gui);
 	g_app.window().addDefaultFileMenu();
-
-	GetSetGui::GetSetTabWidget* intensity=new GetSetGui::GetSetTabWidget("Pre-Processing/Intensity");
-	intensity->show();
-
 	return g_app.exec();
 }
