affea5825c9e8d6ba8480da5264d5ebeda0e1c6e
Andre Aichert <aaichert@gmail.com>
2017-08-18 17:30:58 +0200

    GetSetObjects now has a factory.

--- GetSet/GetSetObjects.cpp
+++ GetSet/GetSetObjects.cpp
@@ -2,19 +2,33 @@
 
 #include "StringUtil.hxx"
 
+
+#include <type_traits>
+#include <stdexcept>
+
 namespace GetSetObjects {
 	
-	Object::Object(GetSetInternal::Section& section, GetSetGui::ProgressInterface& _app)
-		: GetSetInternal::Dictionary::Observer(section.dictionary)
-		, app(_app)
-		, dictionary(section.dictionary)
-		, path(section.path())
+	GetSetGui::ProgressInterface& default_progress_interface() {
+		static GetSetGui::ProgressInterface progress;
+		return progress;
+	}
+	
+	// This is the only place we need direct access to GetSetInternal things. Subclasses need not be aware.
+	Object::Object(const GetSetGui::Section& section, GetSetGui::ProgressInterface *_app)
+		: GetSetInternal::Dictionary::Observer(((GetSetInternal::Section&)section).dictionary)
+		, app(_app?*_app:default_progress_interface())
+		, dictionary(((GetSetInternal::Section&)section).dictionary)
+		, path(((GetSetInternal::Section&)section).path())
 		, ignore_notify(false)
 	{}
 
-	GetSetGui::Section Object::gui_section() { return GetSetGui::Section(path,dictionary); }
+	GetSetGui::Section Object::gui_section() const { return GetSetGui::Section(path,dictionary); }
 
-	void Object::gui_ignore_notify(bool ignore) { ignore_notify=ignore; }
+	void Object::gui_init() {
+		auto *obj=dynamic_cast<Configurable*>(this); if (obj) obj->gui_declare_section(gui_section());
+	}
+
+	void Object::gui_ignore_notify(bool ignore) const { ignore_notify=ignore; }
 	
 	void Object::notify(const GetSetInternal::Node& node, GetSetInternal::Dictionary::Signal signal)
 	{
@@ -25,5 +39,46 @@ namespace GetSetObjects {
 				gui_notify(path==node.super_section?"":node.super_section.substr(path.size()+1), node);
 		}
 	}
+
+// 
+// Object Factory (use is optional)
+// 
+
+	/// Default ProgressInterface istance and pointer
+	GetSetGui::ProgressInterface  default_interface;
+	GetSetGui::ProgressInterface *default_interface_ptr=&default_interface;
+
+	void factory_progress_set_default_interface(GetSetGui::ProgressInterface& app) {
+		default_interface_ptr=&app;
+	}
+
+	GetSetGui::ProgressInterface& factory_progress_default_interface() {
+		return *default_interface_ptr;
+	}
+
+	/// Instance which holds all known objects.
+	std::map<std::string, Object* (*)(const GetSetGui::Section&, GetSetGui::ProgressInterface&)> factory_registration;
+
+	std::set<std::string> factory_known_types() {
+		std::set<std::string> ret;
+		for (auto it=factory_registration.begin();it!=factory_registration.end();++it)
+			ret.insert(it->first);
+		return ret;
+	}
+
+	Object* factory_create(const std::string& class_name, const GetSetGui::Section& section, GetSetGui::ProgressInterface& progress_interface) {
+		auto   it=factory_registration.find(class_name);
+		return it==factory_registration.end() ? 0x0 : it->second(section, progress_interface);
+	}
+	
+	void factory_register_type(const std::string& class_name, Object* (*instantiate)(const GetSetGui::Section&, GetSetGui::ProgressInterface&))
+	{
+		// Make sure that the type class_name is unambigous
+		if (factory_registration.find(class_name)!=factory_registration.end())
+			throw std::runtime_error ( std::string(__FUNCTION__ " : The specified type ") + class_name + " has already been registered!");
+		// Then just register
+		factory_registration[class_name]=instantiate;
+	}
+
 
 } // namespace GetSetObjects
--- GetSet/GetSetObjects.h
+++ GetSet/GetSetObjects.h
@@ -20,40 +20,120 @@
 #ifndef __getset_object_hxx
 #define __getset_object_hxx
 
-#include "GetSet.hxx"
+#include "GetSetIO.h"
+
 #include "ProgressInterface.hxx"
 
-namespace GetSetObjects {
+#include <set>
+#include <sstream>
 
-	/// Interface for a class which can be represented directly by a GetSet section.
-	class Configurable {
+namespace GetSetObjects {
+	
+	/// Interface for a class which can be represented directly by a GetSet Section.
+	struct Configurable {
 	public:
-		virtual void gui_declare_section(GetSetGui::Section&) = 0;
-		virtual void gui_retreive_section(GetSetGui::Section&) = 0;
+		virtual void gui_declare_section (const GetSetGui::Section& ) = 0;
+		virtual void gui_retreive_section(const GetSetGui::Section& ) = 0;
+
+		/// Load this object from an ini-File
+		inline bool gui_load(const std::string& file) {
+			GetSetInternal::Dictionary dict;
+			if (!GetSetIO::load(file,dict))
+				return false;
+			gui_retreive_section(dict);
+			return true;
+		}
+
+		/// Save this object to an ini-File
+		inline bool gui_save(const std::string& file) {
+			GetSetInternal::Dictionary dict;
+			gui_declare_section(dict);
+			return GetSetIO::save(file,dict);
+		}
+
 	};
 
 	/// An interface for objects, which store their settings in a specific Section and are notified for changes relative to that section only.
 	class Object : public GetSetInternal::Dictionary::Observer {
 	protected:
-		GetSetInternal::Dictionary&   dictionary;
-		GetSetGui::ProgressInterface& app;
-		const std::string             path;
-		bool ignore_notify;
+		GetSetInternal::Dictionary&           dictionary;
+		const std::string                     path;
+		mutable GetSetGui::ProgressInterface& app;
+		mutable bool                          ignore_notify;
+
 	public:
-		Object(GetSetInternal::Section& section, GetSetGui::ProgressInterface& app);
+		/// Contructor for all objects. For default ProgressInterface, see factory_use_progress_interface.
+		Object(const GetSetGui::Section& section, GetSetGui::ProgressInterface *app=0x0);
 
 		/// The section where we store our parameters.
-		GetSetGui::Section gui_section();
+		GetSetGui::Section gui_section() const;
 		
+		/// Two-stage construction. By default, tries to cast to Configurable and call gui_declare.
+		virtual void gui_init();
+
 		/// A node in our gui_section() has changed its value. The relative path to where the node resides is passed in section.
-		virtual void gui_notify(const std::string& section, const GetSetInternal::Node&) = 0;
+		virtual void gui_notify(const std::string& relative_section, const GetSetInternal::Node& node) {}
 
 		/// Temporarily ignore notifications
-		void gui_ignore_notify(bool ignore=true);
+		void gui_ignore_notify(bool ignore=true) const ;
+
+		/// Dynamic cast to a specific type
+		template <class Derived> Derived* cast() {return dynamic_cast<Derived*>(this);}
 
 	private:
-		/// Check if this notification concerns a node in gui_section()
+		/// Check if this notification concerns a node in gui_section() and, if so, call gui_notify with its raltive path.
 		void notify(const GetSetInternal::Node& node, GetSetInternal::Dictionary::Signal signal);
+	};
+
+	/// A simple object which lives in a fixed section
+	template <class ConfigurableDerived>
+	struct Struct : public ConfigurableDerived, public Object {
+		Struct(const GetSetGui::Section& section, GetSetGui::ProgressInterface *app=0x0) : Object(section, app) {}
+	};
+
+} // namespace GetSetObjects
+
+// 
+// Object Factory (use is optional)
+// 
+
+// For Human-readable object names across compiliation units and compilers put this in your header.
+#define GETSET_OBJECT_DECLARE(CLASS_NAME) template<> inline std::string typeName<CLASS_NAME>() { return #CLASS_NAME; }
+
+// If you want your GetSetObjects::Object CLASS_NAME registered with the factory, put this in your c++ file.
+#define GETSET_OBJECT_REGISTER(CLASS_NAME)                                                                           \
+	CLASS_NAME * factory_create##CLASS_NAME(const GetSetGui::Section&, GetSetGui::ProgressInterface& )               \
+	{ CLASS_NAME * obj=new CLASS_NAME(section); obj->gui_init(); return obj; }                                       \
+	FactoryRegistration<CLASS_NAME> factory_register##CLASS_NAME(factory_create##CLASS_NAME);
+
+namespace GetSetObjects {
+	/// Set the default ProgressInterface which GetSetTypes use.
+	void factory_progress_set_default_interface(GetSetGui::ProgressInterface&);
+
+	/// Set the default ProgressInterface which GetSetTypes use.
+	GetSetGui::ProgressInterface& factory_progress_default_interface();
+
+	/// A set of known types. To register a type, use GETSET_OBJECT_REGISTER(CLASS_NAME) in your c++ file.
+	std::set<std::string> factory_known_types();
+
+	/// Try to create an object of a specified type. Returns null for unknown types.
+	Object* factory_create(const std::string& class_name, const GetSetGui::Section& section,
+		GetSetGui::ProgressInterface& progress_interface=factory_progress_default_interface());
+
+	/// Try to create an object of a specified type. Returns null for unknown types.
+	inline Object* factory_create(const std::string& class_name) { factory_create(class_name,class_name); }
+
+	// 
+	// Please do not use the following factory_register_type and FactoryRegistration directly.
+	// Instead, use GETSET_OBJECT_REGISTER(CLASS_NAME) in your c++ file.
+	// 
+
+	void factory_register_type(const std::string&, Object* (*)(const GetSetGui::Section&, GetSetGui::ProgressInterface&));
+	template <class GetSetObject>
+	struct FactoryRegistration {
+		FactoryRegistration(Object* (*instantiate)(const GetSetGui::Section&, GetSetGui::ProgressInterface&))
+		                       {register_type(typeName<GetSetObject>(),instantiate,this);}
+		~FactoryRegistration() {register_type(typeName<GetSetObject>(),0x0        ,this);}
 	};
 
 } // namespace GetSetObjects
