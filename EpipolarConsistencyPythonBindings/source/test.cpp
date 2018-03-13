370175ff0822c4725df9de6f53a88a6109118c3e
Stephan Seitz <stephan.lauf@yahoo.de>
2018-03-13 19:26:06 +0100

    - add helpful text on how to convert python::bytes to C++ char*

--- EpipolarConsistencyPythonBindings/source/test.cpp
+++ EpipolarConsistencyPythonBindings/source/test.cpp
@@ -9,6 +9,12 @@
 namespace py = pybind11;
 
 
+// Passing bytes to C++
+// 
+// A Python bytes object will be passed to C++ functions that accept std::string or char* without conversion.
+
+
+
 PYBIND11_MODULE(_epipolar_consistency, m) {
     m.def("makeProjectionMatrix", &Geometry::makeProjectionMatrix, "Makes a projections matrix.");
     m.def("makeCalibrationMatrix", &Geometry::makeCalibrationMatrix, "Makes a calibration matrix.");
